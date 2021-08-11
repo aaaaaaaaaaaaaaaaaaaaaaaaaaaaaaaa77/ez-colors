2e660e2bfab4d3a040be996bc1466605044635b8
aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa77 <77732738+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa77@users.noreply.github.com>
2021-08-11 13:46:01 +0300

    📂| Added package files via upload

---
+++ index.js
@@ -0,0 +1,74 @@
+const colors = require("./colors.json");
+
+module.exports = {
+  color(color) {
+    if (color === "YELLOW") {
+      color = colors.YELLOW
+      return color;
+    } 
+     else if (color === "RED") {
+      color = colors.RED
+      return color;
+    } 
+     else if (color === "PINK") {
+      color = colors.PINK
+      return color;
+    } 
+     else if (color === "VIOLET") {
+      color = colors.VIOLET
+      return color;
+    }
+     else if (color === "BLUE") {
+      color = colors.BLUE
+      return color;
+    }
+     else if (color === "GREEN") {
+      color = colors.GREEN
+      return color;
+    }
+     else if (color === "BROWN") {
+      color = colors.BROWN
+      return color;
+    }
+     else if (color === "GRAY") {
+      color = colors.GRAY
+      return color;
+    }
+     else if (color === "MELLOW") {
+      color = colors.MELLOW
+      return color;
+    }
+     else if (color === "GOLD") {
+      color = colors.GOLD
+      return color;
+    }
+     else if (color === "SALMON") {
+      color = colors.SALMON
+      return color;
+    }
+     else if (color === "RUBY") {
+      color = colors.RUBY
+      return color;
+    }
+     else if (color === "HIBISCUS") {
+      color = colors.HIBISCUS
+      return color;
+    }
+     else if (color === "DENIM") {
+      color = colors.DENIM
+      return color;
+    }
+     else if (color === "FOREST") {
+      color = colors.FOREST
+      return color;
+    }
+     else if (color === "CEDAR") {
+      color = colors.CEDAR
+      return color;
+    }
+     else if (color === "FOSSIL") {
+      color = colors.FOSSIL
+      return color;
+    }
+  }
+}
